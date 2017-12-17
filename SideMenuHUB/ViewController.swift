a58ff6b5b2c91eec802644b5663ec846249fe978
Daniel Yo <dnl992004@yahoo.com.au>
2017-12-17 13:02:45 -0700

    Initial Commit

---
+++ SideMenuHUB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideMenuHUB
+//
+//  Created by Daniel Yo on 12/17/17.
+//  Copyright © 2017 Daniel Yo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
